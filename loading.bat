e6f46f9320b092bc7386ed47b653c0b5eeb7bedc
Syntax <dylankwc@gmail.com>
2018-03-02 21:25:03 -0500

    Initial commit

---
+++ loading.bat
@@ -0,0 +1,29 @@
+@echo off
+title L O A D I N G . . .
+
+color 1a
+echo                                    L O A D I N G
+ping localhost -n 2 >nul
+cls
+echo                                    L O A D I N G .
+ping localhost -n 2 >nul
+cls
+echo                                    L O A D I N G . .
+ping localhost -n 2 >nul
+cls
+echo                                    L O A D I N G . . .
+ping localhost -n 2 >nul
+cls
+echo                                    L O A D I N G
+ping localhost -n 2 >nul
+cls
+echo                                    L O A D I N G .
+ping localhost -n 2 >nul
+cls
+echo                                    L O A D I N G . .
+ping localhost -n 2 >nul
+cls
+echo                                    L O A D I N G . . .
+ping localhost -n 2 >nul
+cls
+pause
---
+++ test.vbs
@@ -0,0 +1,2 @@
+X=MsgBox("Welcome to Syntax's Public USB, Continue?", 4+32, "Welcome")
+X=MsgBox("Please do not delete the contents of this USB, Accept?", 0+32, "Accept?")
